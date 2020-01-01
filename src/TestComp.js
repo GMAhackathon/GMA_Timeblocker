a179f07b003040cf21fb82ec5a88bbed2a7d3c56
Nathan Loveless <nathansl2003@yahoo.com>
2020-01-01 15:47:16 -0500

    Initial project build out

---
+++ src/TestComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestComp = () => {
+  return <div>Your are inside TestComp.js!</div>;
+};
+
+export default TestComp;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TestComp from "./TestComp";
+
+const App = () => {
+  return <TestComp />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
