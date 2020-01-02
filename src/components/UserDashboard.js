33b7f22e701be73239ea7031bb78ad0800ce3849
Nathan Loveless <nathansl2003@yahoo.com>
2020-01-02 16:00:50 -0500

    Setup components UserDashboard.js/CalendarCard.js ready for data retrival

---
+++ src/components/UserDashboard.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import CalendarCard from "./CalanderCard";
+
+const UserDashboard = props => {
+  const [user, setUser] = useState(props);
+
+  useEffect(() => {
+    // Api Call to logged in User
+    f;
+  });
+
+  // Check if reservation exists
+  // If Exists, show reservation
+  // If !Exists call Calendar Appointment
+  if (user.hasReservation) {
+    render(<div>You have a reservation</div>);
+  } else {
+    return (
+      <div>
+        <CalendarCard />
+      </div>
+    );
+  }
+};
+
+export default UserDashBoard;
