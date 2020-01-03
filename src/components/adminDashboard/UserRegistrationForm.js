3d9abdb0e85d7b864aad4e39f32a5deee41333e5
Nathan Loveless <nathansl2003@yahoo.com>
2020-01-03 10:16:32 -0500

    playing in admin

--- src/components/adminDashboard/UserRegistrationForm.js
+++ src/components/adminDashboard/UserRegistrationForm.js
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 // import moment from 'moment';
-import { Spin } from 'antd';
+import { Spin } from "antd";
 
 const FormWrap = styled.form`
   // background: #EDEEEF;
@@ -11,7 +11,7 @@ const FormWrap = styled.form`
   display: flex;
   flex-direction: column;
   transition: all 200ms ease;
-`
+`;
 
 const Input = styled.input`
   outline: none;
@@ -20,7 +20,7 @@ const Input = styled.input`
   background: white;
   width: 100%;
   height: 26px;
-`
+`;
 
 const Button = styled.button`
   width: 120px;
@@ -32,27 +32,61 @@ const Button = styled.button`
   color: white;
   outline: none;
   cursor: pointer;
-`
+`;
 
+const AddChildButton = styled.button`
+  display: flex;
+  width: 75px;
+  height: 25px;
+  border-radius: 3px;
+  margin: 5px 5px 0px 0px;
+  background: #50a76d;
+  text-align: center;
+  color: white;
+  outline: none;
+  cursor: pointer;
+`;
+
+const ChildrenContainer = styled.div`
+  text-align: center;
+  font-weight: bold;
+`;
 
 function UserRegistrationForm(props) {
-  const [user, setUser] = useState({firstName: '', lastName: '', email: '', familySize: ''});
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    familySize: "",
+    student: ""
+  });
 
   // display a spinner on isLoading when posting a new record
   const [loading, setLoading] = useState(props.createNewStudentIsLoading);
+  const [student, setStudent] = useState([]);
+  const [childsName, setChildsName] = useState("");
 
-  useEffect(() => {
-
-  }, [loading])
-
+  useEffect(() => {}, [loading]);
 
   function handleChange(event) {
-    setUser({ ...user, [event.target.name]: event.target.value })
-  }                                        
+    setUser({ ...user, [event.target.name]: event.target.value });
+  }
+
+  function handleChildChange(event) {
+    setChildsName(event.target.value);
+  }
+
+  function addChild(event) {
+    setStudent([childsName]);
+    console.log("nl: UserRegistrationForm.js: addChild: user: ", student);
+    console.log(
+      "nl: UserRegistrationForm.js: addChild: childsName: ",
+      childsName
+    );
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-
   }
 
   function handleCancel(event) {
@@ -60,66 +94,104 @@ function UserRegistrationForm(props) {
     props.handleCancelButtonOnForm();
   }
 
-
-      return (
-        <FormWrap onSubmit={handleSubmit} style={{margin: '30px 10px 20px 10px'}}>
-          <fieldset style={{border: '1px solid transparent', margin: '10px 5px 0px 5px',  background: '#d8dee0'}}>
-            <div style={{display: 'grid', textAlign: 'left', gridTemplateColumns: '1fr 1fr 1fr 1fr',
-                         gridGap: '10px', margin: '10px'}}>
-              <div >
-                <label>First Name</label>
-                <div>
-                  <Input
-                    type="text"
-                    name="firstName"
-                    value={user.firstName}
-                    onChange={handleChange} />
-                </div>
-              </div>
-              <div>
-                <label>Last Name</label>
-                <div>
-                  <Input
-                    type="text"
-                    name="lastName"
-                    value={user.lastName}
-                    onChange={handleChange} />
-                </div>
-              </div>
-              <div style={{gridColumn: 'span 2'}}>
-                <label>Email</label>
-                <div>
-                  <Input 
-                    style={{width: '100%'}}
-                    type="text"
-                    name="email"
-                    value={user.email}
-                    onChange={handleChange} />
-                </div>
-              </div>
-              <div style={{gridColumn: 'span 2'}}>
-                <label>Family Size</label>
-                <div>
-                  <Input 
-                    style={{width: '100%'}}
-                    type="text"
-                    name="familySize"
-                    value={user.familySize}
-                    onChange={handleChange} />
-                </div>
-              </div>
+  return (
+    <FormWrap onSubmit={handleSubmit} style={{ margin: "30px 10px 20px 10px" }}>
+      <fieldset
+        style={{
+          border: "1px solid transparent",
+          margin: "10px 5px 0px 5px",
+          background: "#d8dee0"
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            textAlign: "left",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gridGap: "10px",
+            margin: "10px"
+          }}
+        >
+          <div>
+            <label>First Name</label>
+            <div>
+              <Input
+                type="text"
+                name="firstName"
+                value={user.firstName}
+                onChange={handleChange}
+              />
             </div>
-          </fieldset>
-          <div style={{alignSelf: 'flex-end'}}>
-            <Button onClick={handleCancel} style={{background: '#C73642', width: '80px'}}>
-              Cancel
-            </Button>
-            <Button type="submit">
-              Add student
-            </Button>
           </div>
-        </FormWrap>
-      )
+          <div>
+            <label>Last Name</label>
+            <div>
+              <Input
+                type="text"
+                name="lastName"
+                value={user.lastName}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div style={{ gridColumn: "span 2" }}>
+            <label>Email</label>
+            <div>
+              <Input
+                style={{ width: "100%" }}
+                type="text"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div style={{ gridColumn: "span 2" }}>
+            <label>Family Size</label>
+            <div>
+              <Input
+                style={{ width: "100%" }}
+                type="text"
+                name="familySize"
+                value={user.familySize}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div style={{ gridColumn: "span 1" }}>
+            <label>Child's Name</label>
+            <div>
+              <Input
+                style={{ width: "100%" }}
+                type="text"
+                name="name"
+                value={childsName}
+                onChange={handleChildChange}
+              />
+            </div>
+            <AddChildButton onClick={addChild}>Add Child</AddChildButton>
+          </div>
+          <div style={{ gridColumn: "span 1" }}>
+            <ChildrenContainer>
+              <h3>Children</h3>
+              {/* {student.map(item => (
+                <div>{item.name}</div>
+              ))} */}
+            </ChildrenContainer>
+          </div>
+        </div>
+      </fieldset>
+      <div style={{ alignSelf: "flex-end" }}>
+        <Button
+          onClick={handleCancel}
+          style={{ background: "#C73642", width: "80px" }}
+        >
+          Cancel
+        </Button>
+        <Button type="submit">Add student</Button>
+      </div>
+    </FormWrap>
+  );
 }
 
 export default UserRegistrationForm;
