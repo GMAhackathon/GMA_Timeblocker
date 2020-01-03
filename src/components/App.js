9814ee990b993f2dda77b810961df466b9e470bd
Rayven <rayven.burns@gmail.com>
2020-01-03 01:16:28 -0500

    added register component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from './login'
+import Register from './Register';
 
 function App() {
 
@@ -12,12 +13,13 @@ function App() {
         <div>
             
             <Link to = "/login">Login</Link>
+            <Link to = "/register">Register</Link>
         </div>
 
 
         <Switch>
             <Route path = "/login" component = {Login} />
-           
+           <Route path = "/register" component = {Register} />
         </Switch>
 
         </Router>
---
+++ src/components/Register.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import styled from 'styled-components';
+
+const Register = (props) => {
+
+    const [credentials, setCredentials] = useState({
+
+        email: '',
+        password: '', 
+        firstname: '',
+        lastname: '',
+        studentname: ''
+    });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...credentials, 
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        axiosWithAuth()
+        .post('/register', credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/login') //or whatever endpoint user dashboard is
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    return(
+        <StyledDiv>
+            
+            <StyledText>Please Sign Up</StyledText>
+
+            <form onSubmit = {handleSubmit}>
+
+                <StyledInput
+                    type = "text"
+                    name = "firstname"
+                    placeholder = "Parent's First Name"
+                    onChange = {handleChange}
+                    value = {credentials.firstname}
+                />
+
+                <StyledInput
+                    type = "text"
+                    name = "lastname"
+                    placeholder = "Last Name"
+                    onChange = {handleChange}
+                    value = {credentials.lastname}
+                />
+
+                <StyledInput
+                    type = "text"
+                    name = "email"
+                    placeholder = "Email"
+                    onChange = {handleChange}
+                    value = {credentials.email}
+                />
+
+                <StyledInput
+                    type = "password"
+                    name = "password"
+                    placeholder = "Password"
+                    onChange = {handleChange}
+                    value = {credentials.password}
+                />
+
+                <StyledInput
+                    type = "text"
+                    name = "studentname"
+                    placeholder = "Student's Name"
+                    onChange = {handleChange}
+                    value = {credentials.studentname}
+                />
+
+                <StyledButton>Submit</StyledButton>
+            </form>
+        </StyledDiv>
+    )
+};
+
+export default Register;
+
+
+const StyledDiv = styled.div`
+    margin-left: 20%;
+    margin-top: 10%;
+    background: #007B2B;
+    box-sizing: border-box;
+    margin-right: 37%; 
+    padding-left: 15%;
+    padding-bottom: 5%;
+    width: 50%;
+    border-radius: 9px;
+`
+const StyledInput = styled.input`
+    display: flex;
+    margin-bottom: 5%;
+    padding: 1.1%;
+    border-radius: 2px;
+    border: 1px solid lightgrey;
+    font-size: 1rem;
+    width: 40%;
+    margin-left: 4%;
+`
+
+const StyledButton = styled.button`
+    padding: 2%;
+    width: 30%;
+    border-radius: 3px;
+    font-size: .8rem;
+    margin-left: 9%;
+`
+const StyledText = styled.h1`
+    padding-top: 5%;
+    
+`
